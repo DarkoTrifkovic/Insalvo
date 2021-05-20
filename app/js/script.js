ba5298fca5b5c35bcb31f92d4b829ac37acbd018
Darko Trifkovic <trifkovicdarko.n@gmail.com>
2021-05-20 23:08:48 +0200

    section faq end

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,26 @@
-const testString = 'This is a test!';
 
-console.log(testString);
+$(document).ready(() => {
+    $('.js-banner__carousel, .js-testimonials__carousel').slick({
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+        // fade: true,
+        arrows: false
+    });
 
-const fruits = [
-  'mango',
-  'papaya',
-  'lychee',
-];
-
-fruits.map((fruit) => {
-  console.log(fruit);
+    $('.faq__card-header').click(function () {
+		// self clicking close
+		if ($(this).next('.faq__card-body').hasClass('active')) {
+			$(this).next('.faq__card-body').removeClass('active').slideUp();
+			$(this).children('i').removeClass('faq__down').addClass('faq__right');
+		} else {
+			$('.faq__card .faq__card-body').removeClass('active').slideUp();
+			$('.faq__card .faq__card-header i').removeClass('faq__down').addClass('faq__right');
+			$(this).next('.faq__card-body').addClass('active').slideDown();
+			$(this).children('i').removeClass('faq__right').addClass('faq__down');
+      
+		}
+	});
 });
+
+
